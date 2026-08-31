c91598a9c2c69aea6965f9a58c3025aad6c2e5e7
Bilal <14672@holbertonstudents.com>
2026-08-31 12:26:02 +0200

    Fix SyntaxError in structured_output.py

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-Language="Language:python"
-Version="Version:3")
-f"{pi=3.14159:.2f}"
-computation_valid=10>2
-print("language")
-print("Version")
-print(f"{pi:.2f}")
-print(f"Computation vald: {computation_valid}")
+pi = 3.14159
+is_valid = 10 > 2
 
+print("Language: Python")
+print("Version: 3")
+print(f"Pi approx: {pi:.2f}")
+print(f"Computation valid: {is_valid}")
