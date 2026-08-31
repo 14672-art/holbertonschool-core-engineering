df4434be08749384576744109d8231d8aee9d849
Bilal <14672@holbertonstudents.com>
2026-08-31 12:20:38 +0200

    Add structured_output.py

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+Language="Language:python"
+Version="Version:3")
+f"{pi=3.14159:.2f}"
+computation_valid=10>2
+print("language")
+print("Version")
+print(f"{pi:.2f}")
+print(f"Computation vald: {computation_valid}")
+
