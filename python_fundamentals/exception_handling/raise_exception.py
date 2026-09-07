95cd724bad1e5e6177e1a7f1488371c89f7c73b9
Bilal <14672@holbertonstudents.com>
2026-09-07 15:06:04 +0200

    Add raise_exception function

---
+++ python_fundamentals/exception_handling/raise_exception.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module pour lever explicitement une exception TypeError.
+"""
+
+
+def raise_exception():
+    """
+    Lève une exception de type TypeError.
+    """
+    raise TypeError
