940bd7416c7ccf97cbd0234d6ba6a42681a3ebfa
Bilal <14672@holbertonstudents.com>
2026-09-09 12:21:21 +0200

    2. Size validation

---
+++ python_oop/classes_and_object_model/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module qui définit la classe Square."""
+
+
+class Square:
+    """Représente un carré."""
+
+    def __init__(self, size=0):
+        """Initialise une nouvelle instance de Square avec validation de taille.
+
+        Args:
+            size (int): La taille du côté du carré (par défaut 0).
+
+        Raises:
+            TypeError: Si size n'est pas un entier.
+            ValueError: Si size est inférieur à 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
