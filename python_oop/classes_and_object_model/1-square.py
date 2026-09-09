356a7fe6a97bfce568fbabeadb3fc4c239d24a79
Bilal <14672@holbertonstudents.com>
2026-09-09 12:19:42 +0200

    1. Square with size

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module qui définit la classe Square."""
+
+
+class Square:
+    """Représente un carré."""
+
+    def __init__(self, size):
+        """Initialise une nouvelle instance de Square.
+
+        Args:
+            size: La taille du côté du carré.
+        """
+        self.__size = size
