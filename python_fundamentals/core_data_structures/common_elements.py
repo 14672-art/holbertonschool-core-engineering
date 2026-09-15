9e2793df1bf696fdc972e0c84c774571de92610e
Bilal <14672@holbertonstudents.com>
2026-09-15 10:08:26 +0200

    Add common_elements function

---
+++ python_fundamentals/core_data_structures/common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module pour trouver les éléments communs entre deux ensembles."""
+
+
+def common_elements(set_1, set_2):
+    """Renvoie un ensemble contenant les éléments communs à set_1 et set_2."""
+    return set_1 & set_2
