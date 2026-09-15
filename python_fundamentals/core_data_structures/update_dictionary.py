58cbfb2f300bdad8789a92322b80d0490fea7ffd
Bilal <14672@holbertonstudents.com>
2026-09-15 10:10:23 +0200

    Add update_dictionary function

---
+++ python_fundamentals/core_data_structures/update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module pour mettre à jour ou ajouter une clé/valeur dans un dictionnaire."""
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Ajoute ou met à jour une paire clé/valeur dans a_dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
